cb36bda1491deec11a3f793d67e261fd36349481
ParameswarSuresh <75156193+ParameswarSuresh@users.noreply.github.com>
2021-02-10 17:17:58 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,13 +1,14 @@
 class Ball{
-    constructor(x, y, radius) {
+    constructor(x, y, width, height) {
     var options = {
         restitution:0.3,
         friction:0.5,
         density:1.2
 
     }
-    this.body = Bodies.circle(x, y, 25, options);
-    this.radius = 25;
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height
 
     World.add(world, this.body);
 }
@@ -17,9 +18,8 @@ display(){
     push();
     translate(pos.x, pos.y);
     rotate(angle);
-    ellipseMode(CENTER);
     fill("pink");
-    ellipse(0, 0, this.radius);
+    ellipse(0, 0, this.width, this.height);
     pop();
 
      }
